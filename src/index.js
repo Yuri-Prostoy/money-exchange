9f6ec119b34541f253de1692aa3ca519ac4108bc
Yuri <zhukovscy0@mail.ru>
2019-03-24 14:08:56 +0300

    complete money-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+    let answer = {};
+
+    if (currency > 10000){
+      answer["error"]="You are rich, my friend! We don't have so much coins for exchange";
+    }
+   else{
+     if(Math.floor(currency/50) >0){
+      answer["H"] = Math.floor(currency/50);
+     currency = currency%50;
+    }
+     
+    if(Math.floor(currency/25)>0){
+      answer["Q"] = Math.floor(currency/25);
+      currency = currency%25;
+    }
+    
+      if(Math.floor(currency/10)>0){
+      answer["D"] = Math.floor(currency/10);
+      currency = currency%10;
+    }
+    
+      if(Math.floor(currency/5)>0){
+      answer["N"] = Math.floor(currency/5);
+      currency = currency%5;
+    }
+    
+    if(currency>0){
+      answer["P"] = currency;
+    }
+  }
+
+    return answer;
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
